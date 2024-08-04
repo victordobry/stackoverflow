17ba3fc75c09a4d8c2184b324f685871cf6dbe21
Victor Dombrovskiy <victordobry@icloud.com>
2024-08-04 17:54:55 +0700

    [78830744] Add complete working example

---
+++ 78830744/go.mod
@@ -0,0 +1,3 @@
+module bitbucket.org/foo/ppprint.git
+
+go 1.22.1
---
+++ 78830744/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	be "bitbucket.org/foo/ppprint.git/internal/backends"
+)
+
+func main() {
+	fmt.Println(be.Hello("Gladys"))
+}
